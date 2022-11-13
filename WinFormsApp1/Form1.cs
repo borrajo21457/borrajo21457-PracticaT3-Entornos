288ab4ee6d99ca9be03c1e96e6fc9f133612b39f
fuxinito <pwai_wbdax12@tijux.com>
2022-11-13 12:55:38 +0100

    Correccion de codigo

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,35 +7,42 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       private void button1_Click(object sender, EventArgs e)
         {
-            string textoTelegrama;
+            string textoTelegrama_OskarBorr22_23;
             char tipoTelegrama = ' ';
             int numPalabras = 0;
-            double coste;
+            double coste_OskarBorr22_23;
+
             //Leo el telegrama
-            textoTelegrama = txtTelegrama.Text;
+            textoTelegrama_OskarBorr22_23 = txtTelegrama.Text;
             // telegrama urgente?
             if (cbUrgente.Checked)
+            {
                 tipoTelegrama = 'u';
+            }
+            else tipoTelegrama = 'o';
+
             //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            string[] cortarPalabras = textoTelegrama_OskarBorr22_23.Split(' ');
+            numPalabras = cortarPalabras.Length;
+
             //Si el telegrama es ordinario
-            if (tipoTelegrama == 'o')
+            if (tipoTelegrama == 'o' && numPalabras > 0)
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste_OskarBorr22_23 = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    coste_OskarBorr22_23 = 0.5 * numPalabras;
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
-                    coste = 5;
+                    coste_OskarBorr22_23 = 5;
                 else
-                    coste = 5 + 0.75 * (numPalabras - 10);
+                    coste_OskarBorr22_23 = 5 + 0.75 * (numPalabras - 10);
             else
-                coste = 0;
-            txtPrecio.Text = coste.ToString() + " euros";
+                coste_OskarBorr22_23 = 0;
+            txtPrecio.Text = coste_OskarBorr22_23.ToString() + " euros";
         }
     }
 }
